a9d69c6133a9fb5dee35838483cdd607e5188deb
M Abdullah <192000863+AbdullahXDeveloper@users.noreply.github.com>
2026-06-06 11:57:16 +0500

    updated- Main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
+from analysis import (
+    load_data,
+    basic_stats,
+    high_risk_patients,
+    age_group_analysis
+)
 
-import numpy
+from visualization import plots
 
-print("============[ Medical DA System ]============")
+def main():
+    data = load_data("data/patients.csv")
 
+    print("\n=== BASIC STATS ===")
+    basic_stats(data)
+
+    print("\n=== HIGH RISK PATIENTS ===")
+    high_risk_patients(data)
+
+    print("\n=== AGE GROUP ANALYSIS ===")
+    age_group_analysis(data)
+
+    plots(data)
+
+if __name__ == "__main__":
+    main()
