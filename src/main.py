2a9aedaf114fce4426ea3185b473e043fba56c7f
M Abdullah <192000863+AbdullahXDeveloper@users.noreply.github.com>
2026-06-06 12:09:03 +0500

    Updated- UI (main.py)

--- src/main.py
+++ src/main.py
@@ -4,24 +4,52 @@ from analysis import (
     high_risk_patients,
     age_group_analysis
 )
+
 from visualization import plots
 from utils import input_data
 
-input_data()
 
 def main():
-    data = load_data("data/patients.csv")
 
-    print("\n=== BASIC STATS ===")
-    basic_stats(data)
+    while True:
 
-    print("\n=== HIGH RISK PATIENTS ===")
-    high_risk_patients(data)
+        print("\n========== Hospital Data Analytics System ==========")
+        print("1. Add New Patient")
+        print("2. Basic Statistics")
+        print("3. High Risk Patients")
+        print("4. Age Group Analysis")
+        print("5. Generate Graphs")
+        print("6. Exit")
 
-    print("\n=== AGE GROUP ANALYSIS ===")
-    age_group_analysis(data)
+        choice = input("\nEnter your choice: ")
 
-    plots(data)
+        if choice == "1":
+            input_data()
+
+        elif choice == "2":
+            data = load_data("data/patients.csv")
+            basic_stats(data)
+
+        elif choice == "3":
+            data = load_data("data/patients.csv")
+            high_risk_patients(data)
+
+        elif choice == "4":
+            data = load_data("data/patients.csv")
+            age_group_analysis(data)
+
+        elif choice == "5":
+            data = load_data("data/patients.csv")
+            plots(data)
+            print("Graphs generated successfully!")
+
+        elif choice == "6":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid choice.")
+
 
 if __name__ == "__main__":
     main()
